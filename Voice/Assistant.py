c9e26fd345657e9808c73db5a6b00c3adfe3583e
YaLameR <reaf_ivan@mail.ru>
2021-04-25 15:18:51 +0300

    VAN v2

--- Voice/Assistant.py
+++ Voice/Assistant.py
@@ -1,5 +1,6 @@
-from playsound import playsound
-import os, psutil
+import os
+import psutil
+
 from data import db_session
 from data.users import User
 
@@ -24,32 +25,29 @@ class Nataly():
         return f'{self.name.title()} : {self.answer_list[number]}'
 
     def function(self):
-        print('Список команд')
-        print('-------------------------------------------------------')
-        print(f'Показ гороскопа. Пример : "{self.name} открой гороскоп на сегодня"')
-
-        print(f'Запуск магического шара: Пример : "{self.name} открой магический шар"')
-
-        print(f'--------------Команды для Админа-----------------------')
-
-        print(
-            f'Информация о пользователях : Пример : "{self.name} покажи информацию пользователя с id 1"')
-
-        print(f'Удаление пользователей : Пример : "{self.name} удали пользователя с id 1')
-
-        print('-------------------------------------------------------')
+        return (f'Список команд\n'
+        ' ------------------------------------------------------- \n' \
+        f'Показ гороскопа. Пример : "{self.name.capitalize()} открой гороскоп на сегодня"\n' \
+        f' Запуск магического шара: Пример : "{self.name.capitalize()} открой магический шар"\n' \
+        ' --------------Команды для Админа-----------------------\n' \
+        f' Информация о пользователях : Пример : "{self.name.capitalize()} покажи информацию пользователя с id 1"\n' \
+        f' Удаление пользователей : Пример : "{self.name.capitalize()} удали пользователя с id 1"\n' \
+        '-------------------------------------------------------')
 
     def open(self, text):
         if text == 'сегодня':
             os.startfile(f"http://127.0.0.1:5000/today")
+            return 'Открываю...'
         elif text == 'завтра':
             os.startfile(f"http://127.0.0.1:5000/tomorrow")
+            return 'Открываю...'
 
     def show_user(self, id):
         if self.admin:
             os.startfile(f'http://127.0.0.1:5000/api/users_data/{id}')
+            return f'Информация о пользователе с id "{id}" показана'
         else:
-            print('У вас недостаточно прав')
+            return 'У вас недостаточно прав'
 
     def delete_user(self, id):
         if self.admin:
@@ -58,10 +56,11 @@ class Nataly():
                 user = db_sess.query(User).filter(User.id == id).first()
                 db_sess.delete(user)
                 db_sess.commit()
+                return f'Пользователь с id "{id}" удален'
             else:
-                print('Данного пользователя не существует')
+                return 'Данного пользователя не существует'
         else:
-            print('У вас недостаточно прав')
+            return 'У вас недостаточно прав'
 
     def close(self, text):
         for process in (process for process in psutil.process_iter() if
--- Voice/voice_main.py
+++ Voice/voice_main.py
@@ -26,57 +26,64 @@ def auth():
         password = input()
     nat.admin = user.admin
 
-
-def aswer(number):
-    answer_list = {
-        1: "Привет",
-        2: "Открываю",
-        3: "Запускаю",
-        4: "Ищу",
-    }
-    return f'Наташа:{answer_list[number]}'
-
-
-def record_volume():
-    r = sr.Recognizer()
-    flag = False
-    with sr.Microphone(device_index=1) as source:
-        r.adjust_for_ambient_noise(source, duration=1)  # настройка посторонних шумов
-        audio = r.listen(source)
-    try:
-        query = r.recognize_google(audio, language='ru-RU')
-        Text = query.lower().split(' ')
-        if nat.name in Text:
-            del Text[Text.index(nat.name)]
-            flag = True
-        if flag:
-            if Text[0] == 'привет':
-                print(nat.aswer(1))
-                playsound('Files\Voice Answers\Hi.mp3')
-            elif Text[0] == 'открой':
-                nat.open(Text[-1])
-            elif Text[0] == 'покажи':
-                nat.show_user(Text[-1])
-            elif Text[0] == 'удали':
-                nat.delete_user(Text[-1])
-            elif Text == ['что', 'ты', 'умеешь']:
-                nat.function()
-            elif Text[:-1] == ['смени', 'имя', 'на']:
-                nat.change_name(Text[-1])
-            else:
-                pass
-    except:
-        pass
-
-
 class Example(QMainWindow):
 
     def __init__(self):
         super().__init__()
         uic.loadUi('Interface/interface_natasha.ui', self)
         self.setWindowTitle('VoiceAssistant')
-        self.run_voice.clicked.connect(record_volume)
+        self.run_voice.clicked.connect(self.record_volume)
         self.running = False
+
+    def aswer(self,number):
+        answer_list = {
+            1: "Привет",
+            2: "Открываю",
+            3: "Запускаю",
+            4: "Ищу",
+        }
+        return f'Наташа:{answer_list[number]}'
+
+    def record_volume(self):
+        r = sr.Recognizer()
+        flag = False
+        with sr.Microphone(device_index=1) as source:
+            r.adjust_for_ambient_noise(source, duration=1)  # настройка посторонних шумов
+            audio = r.listen(source)
+        try:
+            query = r.recognize_google(audio, language='ru-RU')
+            Text = query.lower().split(' ')
+            if nat.name in Text:
+                self.plainTextEdit.appendPlainText('Вы:')
+                self.plainTextEdit.appendPlainText(' '.join(Text).capitalize())
+                del Text[Text.index(nat.name)]
+                flag = True
+            if flag:
+                if Text[0] == 'привет':
+                    print(nat.aswer(1))
+                    playsound('Files\Voice Answers\Hi.mp3')
+                elif Text[0] == 'открой':
+                    self.plainTextEdit.appendPlainText(f'{nat.name.capitalize()}:')
+                    self.plainTextEdit.appendPlainText(nat.open(Text[-1]))
+                elif Text[0] == 'покажи':
+                    self.plainTextEdit.appendPlainText(f'{nat.name.capitalize()}:')
+                    self.plainTextEdit.appendPlainText(nat.show_user(Text[-1]))
+                elif Text[0] == 'удали' or Text[0] == 'удалить':
+                    self.plainTextEdit.appendPlainText(f'{nat.name.capitalize()}:')
+                    self.plainTextEdit.appendPlainText(nat.delete_user(Text[-1]))
+                elif Text == ['что', 'ты', 'умеешь']:
+                    self.plainTextEdit.clear()
+                    self.plainTextEdit.appendPlainText(f'{nat.name.capitalize()}:')
+                    self.plainTextEdit.appendPlainText(nat.function())
+                elif Text[:-1] == ['смени', 'имя', 'на']:
+                    nat.change_name(Text[-1])
+                elif Text[0] == 'очисть' or Text[0] == 'очистить':
+                    self.plainTextEdit.clear()
+                    self.plainTextEdit.appendPlainText('Чат очищен....')
+                else:
+                    self.plainTextEdit.appendPlainText('Неизвестная команда')
+        except:
+            pass
 
 
 if __name__ == '__main__':
